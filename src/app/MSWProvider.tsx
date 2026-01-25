9d025be4ad676101109c5a5cd563868f105a469d
hoon <dudrn587@gmail.com>
2026-01-25 20:24:39 +0900

    feat: 서버 사이드 모킹, 클라이언트 모킹 테스트

--- src/app/MSWProvider.tsx
+++ src/app/MSWProvider.tsx
@@ -1,26 +1,34 @@
 'use client'
 
-import { useState, useEffect, ReactNode } from 'react'
+import { Suspense, use } from 'react'
 
-export default function MSWProvider({ children }: { children: ReactNode }) {
-  const [isMockingReady, setMockingReady] = useState(false)
-
-  useEffect(() => {
-    const init = async () => {
-      if (
-        process.env.NODE_ENV === 'development' &&
-        typeof window !== 'undefined'
-      ) {
-        const { worker } = await import('../mocks/browser')
+const mockingEnabledPromise =
+  process.env.NODE_ENV === 'development' && typeof window !== 'undefined'
+    ? import('../mocks/browser').then(async ({ worker }) => {
         await worker.start({
-          onUnhandledRequest: 'bypass',
+          onUnhandledRequest(request, print) {
+            if (request.url.includes('_next')) {
+              return
+            }
+            print.warning()
+          },
         })
-      }
-      setMockingReady(true)
-    }
+      })
+    : Promise.resolve()
 
-    init()
-  }, [])
+export default function MSWProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Suspense fallback={null}>
+      <MSWProviderWrapper>{children}</MSWProviderWrapper>
+    </Suspense>
+  )
+}
 
-  return isMockingReady ? children : null
+function MSWProviderWrapper({ children }: { children: React.ReactNode }) {
+  use(mockingEnabledPromise)
+  return children
 }
