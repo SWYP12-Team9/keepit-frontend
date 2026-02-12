41b1ba164f7f626501994587f891ca142522a44f
hyeon-aa <ahhyun1120@naver.com>
2026-02-13 01:15:44 +0900

    feat: colorpicker에서 사용자가 다른 색상 선택하면 화면에 보여지게 수정

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -2,9 +2,9 @@
 
 import { cn } from '@/src/utils/cn'
 import Image from 'next/image'
+import { useState } from 'react'
 import { HexColorPicker } from 'react-colorful'
 import { ColorOption } from './types'
-import { useState } from 'react'
 
 interface ColorPickerProps {
   color: string
@@ -19,11 +19,21 @@ export function ColorPicker({
 }: ColorPickerProps) {
   const [isPickerOpen, setPickerOpen] = useState(false)
 
+  const [customColor, setCustomColor] = useState<string | null>(null)
+
+  const handleCustomColorChange = (newColor: string) => {
+    setCustomColor(newColor)
+    setColor(newColor)
+  }
+
+  const isDefaultOption = colorOptions.some((option) => option.value === color)
+
   return (
     <div className="flex items-center gap-10">
       {colorOptions.map((option) => (
         <button
           key={option.id}
+          type="button"
           onClick={() => setColor(option.value)}
           className={cn(
             'h-30 w-30 cursor-pointer rounded-full',
@@ -33,14 +43,26 @@ export function ColorPicker({
         />
       ))}
 
+      {customColor && !isDefaultOption && (
+        <button
+          type="button"
+          onClick={() => setColor(customColor)}
+          className={cn(
+            'border-blue-dark-hover h-30 w-30 cursor-pointer rounded-full border-2',
+          )}
+          style={{ backgroundColor: customColor }}
+        />
+      )}
+
       <button
+        type="button"
         onClick={() => setPickerOpen(!isPickerOpen)}
         className="relative flex h-30 w-30 cursor-pointer items-center justify-center rounded-full bg-[#D9D9D9]"
       >
         <Image src="/icons/plus.svg" alt="plus" width={14} height={14} />
         {isPickerOpen && (
           <div className="absolute top-full left-0 z-50 mt-8">
-            <HexColorPicker color={color} onChange={setColor} />
+            <HexColorPicker color={color} onChange={handleCustomColorChange} />
           </div>
         )}
       </button>
